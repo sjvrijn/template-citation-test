794262d7e154b4d5f3c0c7ec1daf5995d0a7db38
Sander van Rijn <s.vanrijn@esciencecenter.nl>
2024-08-21 09:16:36 +0200

    initial commit

---
+++ src/my_python_package/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about my_python_package."""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Sander"
+__email__ = "s.vanrijn@esciencecenter.nl"
+__version__ = "0.1.0"
